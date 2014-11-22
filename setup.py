16676b1d3d0cb376cac2d41defdc30f956b637f1
LittleQ <littleq0903@gmail.com>
2014-11-22 15:09:44 +0800

    added declaration

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import django_gcs
+
+setup_options = {
+        'name': 'django-gcloud-storage',
+        'versions': django_gcs.__version__,
+        'packages': find_packages(),
+
+        'author': 'Colin Su',
+        'author_email': 'littleq0903@gmail.com',
+
+        'license': 'MIT',
+        'description': open('./README.md').read(),
+        'url': 'https://github.com/littleq0903/django-gcloud-storage',
+
+        'classifiers': [
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Framework :: Django',
+        ]
+}
+
+setup(**setup_options)
+
+
